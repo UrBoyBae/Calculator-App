bd26585eef6ba78c73dbcb36729a98fd5e6c801c
UrBoyBae <aliakbarabdillah21@gmail.com>
2023-07-04 17:03:31 +0700

    progress 20%

---
+++ src/components/partials/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center drop-shadow-lg cursor-pointer'>
+        1
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/partials/monitor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Monitor() {
+  return (
+    <div className='bg-gray-200 w-full h-16 rounded-md text-2xl flex justify-end items-center px-4'>
+      1250000
+    </div>
+  )
+}
+
+export default Monitor
